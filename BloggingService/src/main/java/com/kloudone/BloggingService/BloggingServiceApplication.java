1ce719097553f5594e9380e05039e38d37836150
User <User@DESKTOP-1B8VO4K>
2020-07-13 17:44:04 +0530

    Spring Boot App starter class.

---
+++ BloggingService/src/main/java/com/kloudone/BloggingService/BloggingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kloudone.BloggingService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggingServiceApplication.class, args);
+	}
+
+}
